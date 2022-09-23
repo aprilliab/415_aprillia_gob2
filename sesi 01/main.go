1697897900fce3d94abf6d5ce0709075ac4f3b7f
aprilliab <114209437+aprilliab@users.noreply.github.com>
2022-09-23 13:34:26 +0700

    Update main.go

--- sesi 01/main.go
+++ sesi 01/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 
-	var name, age = "april", 26
+	var name, age = "april", 22
 
 	fmt.Printf("My name is %s and I am %v years old\n", name, age)
 }
